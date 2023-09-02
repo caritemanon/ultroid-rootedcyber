7853f64f1a5fd6071eb44876729542ff142f0ef1
caritemanon <98228802+caritemanon@users.noreply.github.com>
2023-09-02 15:32:26 +0700

    Update rootedcyber.sh

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -1,7 +1,7 @@
 #wget https://gist.githubusercontent.com/rooted-cyber/143d3602a7b7dc476a9e6a9c4bf59a02/raw/19b5df02adc2b08c25bd10949cea0ad9c6ffeb64/deploy
 #bash depl*
 a() {
-git clone https://github.com/TeamUltroid/Ultroid/
+git clone https://github.com/caritemanon/terpal/
 cd Ultroid
 pip install --upgrade pip
 pip3 install -r req*
